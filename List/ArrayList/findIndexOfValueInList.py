17e522d825cea956656a9f5e8480d601c007211c
amol009jobsearch <67626557+amol009jobsearch@users.noreply.github.com>
2024-02-04 06:40:45 +0530

    add the elements at the specific index

---
+++ List/ArrayList/findIndexOfValueInList.py
@@ -0,0 +1,5 @@
+colorList=["Red","Orange","Green","White","Red"]
+
+print(colorList.index("Red")) #retun the index value of red value i.e 0
+print(colorList.index("Red",2)) #retun the index value of red value starting from 2nd index i.e 4
+print(colorList.index("Red",2,5)) #retun the index value of red value starting from 2nd index  i.e 4
